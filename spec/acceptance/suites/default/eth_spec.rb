7e5d0d4521f5a6cf44c2249d0220c3e7d104a718
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-05-16 15:17:00 -0400

    (SIMP-4009) Add OEL and Puppet 5 Support (#26)
    * (SIMP-4009) Add OEL and Puppet 5 Support
    
    * Fixed a strange issue with the 'service' resource in Puppet 5 that
      required output to be redirected to succeed. I was unable to pinpoint
      exactly what the issue is but did manage to determine a successful
      workaround.
    
    SIMP-4009 #comment update network
    
    * Fix .gitlab-ci.yaml

--- spec/acceptance/suites/default/eth_spec.rb
+++ spec/acceptance/suites/default/eth_spec.rb
@@ -9,40 +9,43 @@ describe 'network::eth class' do
     network::eth { 'br0':
       net_type  => 'Bridge',
       onboot    => true,
-      macaddr   => $facts['macaddress_eth1'],
-      require   => Network::Eth['eth1'],
+      macaddr   => $facts['macaddress_#{@interfaces.last}'],
+      require   => Network::Eth['#{@interfaces.last}'],
       ipaddr    => '10.0.2.20',
       gateway   => '10.0.2.2',
       broadcast => '255.255.255.0',
       dns1      => '8.8.8.8'
     }
-    network::eth { 'eth1':
+
+    network::eth { '#{@interfaces.last}':
       bridge  => 'br0',
-      macaddr => $facts['macaddress_eth1'],
+      macaddr => $facts['macaddress_#{@interfaces.last}'],
     }
     EOF
   }
 
-  context 'on each host' do
-    hosts.each do |host|
+  hosts.each do |host|
+    context "on #{host}" do
+      before(:all) do
+        # All interfaces with bridges and loopback removed
+        @interfaces = pfact_on(host, 'interfaces').split(',').delete_if{|i| i =~ /^(br|lo)/}
+      end
+
       it 'should work with no errors' do
         apply_manifest_on(host, manifest, :catch_failures => true)
         apply_manifest_on(host, manifest, :catch_failures => true)
       end
+
       it 'should be idempotent once stable' do
         apply_manifest_on(host, manifest, :catch_changes => true)
       end
 
-      ['eth0','br0'].each do |nic|
-        it "should have #{nic} as devices" do
-          sleep 15
-          device = on host, "ip address show dev #{nic}"
-          expect(device.stdout).to match(/#{nic}/)
-          expect(device.stdout).to match(/inet/)
-        end
+      it 'should have br0 as a device' do
+        sleep 15
+        device = on host, 'ip address show dev br0'
+        expect(device.stdout).to match(/br0/)
+        expect(device.stdout).to match(/inet/)
       end
-
     end
   end
-
 end
