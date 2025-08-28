0e60aea413aa7d1ad3c6f43cb25ec5d85aa7b377
Steven Pritchard <steven.pritchard@gmail.com>
2025-08-28 03:37:49 -0500

    Clean up for rubocop (#97)
    * Clean up for rubocop
    
    ---------
    
    Co-authored-by: Chris Tessmer <chris.tessmer@onyxpoint.com>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,7 +4,7 @@ describe 'network' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
-        let(:facts){ os_facts }
+        let(:facts) { os_facts }
 
         context 'with default parameters' do
           it { is_expected.to create_class('network') }
@@ -12,16 +12,16 @@ describe 'network' do
 
           if os_facts[:simplib__networkmanager] && os_facts[:simplib__networkmanager][:enabled]
             it { is_expected.to create_class('network::service::network_manager') }
-            it { is_expected.to_not contain_service('network').with_ensure('running') }
-            it { is_expected.to_not create_file('/usr/local/sbin/careful_network_restart.sh').with_content(/"puppet \\\\\(agent\\\\\|apply\\\\\)"/) }
+            it { is_expected.not_to contain_service('network').with_ensure('running') }
+            it { is_expected.not_to create_file('/usr/local/sbin/careful_network_restart.sh').with_content(%r{"puppet \\\\\(agent\\\\\|apply\\\\\)"}) }
             it { is_expected.to contain_service('NetworkManager').with_ensure('running') }
-            it { is_expected.to create_file('/usr/local/sbin/careful_network_manager_restart.sh').with_content(/"puppet \\\\\(agent\\\\\|apply\\\\\)"/) }
+            it { is_expected.to create_file('/usr/local/sbin/careful_network_manager_restart.sh').with_content(%r{"puppet \\\\\(agent\\\\\|apply\\\\\)"}) }
           else
             it { is_expected.to create_class('network::service::legacy') }
             it { is_expected.to contain_service('network').with_ensure('running') }
-            it { is_expected.to create_file('/usr/local/sbin/careful_network_restart.sh').with_content(/"puppet \\\\\(agent\\\\\|apply\\\\\)"/) }
-            it { is_expected.to_not contain_service('NetworkManager').with_ensure('running') }
-            it { is_expected.to_not create_file('/usr/local/sbin/careful_network_manager_restart.sh').with_content(/"puppet \\\\\(agent\\\\\|apply\\\\\)"/) }
+            it { is_expected.to create_file('/usr/local/sbin/careful_network_restart.sh').with_content(%r{"puppet \\\\\(agent\\\\\|apply\\\\\)"}) }
+            it { is_expected.not_to contain_service('NetworkManager').with_ensure('running') }
+            it { is_expected.not_to create_file('/usr/local/sbin/careful_network_manager_restart.sh').with_content(%r{"puppet \\\\\(agent\\\\\|apply\\\\\)"}) }
           end
         end
       end
